3ea651866dc1b83086f033c52b1723759ca919c8
Nadim Chowdhury <nadim-chowdhury@outlook.com>
2023-06-06 21:07:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const userRoute = require("./routes/user");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogbook")
+  .then(() => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server Started on ${PORT}`);
+});
+
+// Developed by Nadim Chowdhury >>> nadim.vercel.app
